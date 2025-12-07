23460dcddc5120ec5829f08a95342fd96f4b1ee3
Black <mdfaisulislam1@gmail.com>
2025-12-08 02:44:34 +0600

    npm install ejs

--- index.js
+++ index.js
@@ -6,5 +6,23 @@ app.listen(3000, ()=> {
 });
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.send("<h1>Welcome, Ali</h1>");
+});
+
+app.get('/order', (req, res) => {
+    res.send("<h1>Order</h1>");
+});
+// one parameter in rout
+app.get('/store/:id', (req, res) => {
+    // res.send("<h1>Store</h1>");
+    res.send(req.params.id);
+});
+// two parameter in rout
+app.get('/book/:name-:id', (req, res) => {
+    // res.send("<h1>Store</h1>");
+    res.send(req.params);
+});
+
+app.get('/hire', (req, res) => {
+    res.send("<h1>Hire</h1>");
 });
