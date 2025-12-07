65f4daf8bbdb9296a48b754cc784399b0975969f
Black <mdfaisulislam1@gmail.com>
2025-12-07 23:26:58 +0600

    New

---
+++ index.js
@@ -0,0 +1,22 @@
+// http.createServer(
+//     function(res,req) {
+//         res.writeHead(200, {"Content-type":"text/html"});
+//         res.end("Hello World!");
+//     }
+// ).listen(8080);
+// const { createServer } = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+console.log('Welcome');
